885f07fd95c044ae11b14d350a234eb846d72dfc
Benjumpin <benjaminozy@hotmail.com>
2026-04-13 17:56:37 +0800

    Fixed test case

--- src/main/java/seedu/address/logic/commands/ViewContactCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewContactCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.List;
 import java.util.Objects;
@@ -81,7 +80,7 @@ public class ViewContactCommand extends Command {
             if (model.getUserProfile().isEmpty()) {
                 throw new CommandException(MESSAGE_NO_PROFILE);
             }
-            model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+            model.updateFilteredPersonList(Person::isUserProfile);
             Person userProfile = model.getUserProfile().get();
             return new CommandResult(MESSAGE_SUCCESS_SELF, false, false, userProfile);
         }
